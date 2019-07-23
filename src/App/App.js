370714c4e996bd6cb07e60bf0b8eb13fdb825f5f
Austin Casey <thepoast@gmail.com>
2019-07-22 20:59:20 -0500

    uninstalled and reinstalled bootstrap b/c of bad initial install

--- src/App/App.js
+++ src/App/App.js
@@ -1,12 +1,74 @@
 import React from 'react';
-import './App.scss';
+import {
+  BrowserRouter, Route, Redirect, Switch,
+} from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-      
-    </div>
+import firebase from 'firebase/app';
+import 'firebase/auth';
+
+import Base from '../components/Base/Base';
+import Logout from '../components/Logout/Logout';
+import Auth from '../components/Auth/Auth';
+
+import fbConnection from '../helpers/data/connection';
+
+fbConnection();
+
+const PublicRoute = ({ component: Component, authed, ...rest }) => {
+  const routeChecker = props => (
+    authed === false
+      ? (<Component {...props} />)
+      : (<Redirect to={{ pathname: '/base', state: { from: props.location } }} />)
   );
+  return <Route {...rest} render={props => routeChecker(props)} />;
+};
+
+const PrivateRoute = ({ component: Component, authed, ...rest }) => {
+  const routeChecker = props => (
+    authed === true
+      ? (<Component {...props} />)
+      : (<Redirect to={{ pathname: '/auth', state: { from: props.location } }} />)
+  );
+  return <Route {...rest} render={props => routeChecker(props)} />;
+};
+
+class App extends React.Component {
+  state = {
+    authed: false,
+  }
+
+  componentDidMount() {
+    this.removeListener = firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        this.setState({ authed: true });
+      } else {
+        this.setState({ authed: false });
+      }
+    });
+  }
+
+  render() {
+    const { authed } = this.state;
+    return (
+    <div className="App">
+      <BrowserRouter>
+          <React.Fragment>
+            <Logout authed={authed}/>
+            <div className='container'>
+              <div className='row'>
+                <Switch>
+                  <PublicRoute path='/auth' component={Auth} authed={authed}/>
+                  <PrivateRoute path='/base' component={Base} authed={authed}/>
+
+                  <Redirect from='*' to='/auth' />
+                </Switch>
+              </div>
+            </div>
+          </React.Fragment>
+        </BrowserRouter>
+    </div>
+    );
+  }
 }
 
 export default App;
